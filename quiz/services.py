4e998a97fb3c536a45a5595b4adfff79bd78f957
Даниил Лебедев <2018-00888@students.ssau.ru>
2021-06-11 16:54:12 +0400

    Result logic added

--- quiz/services.py
+++ quiz/services.py
@@ -9,3 +9,23 @@ class QuizResultService():
 
     def get_result(self) -> float:
         # your code here
+        # найти количество правильных ответов
+        if self.quiz_dto.uuid == self.answers_dto.quiz_uuid:
+            right_answers = 0
+            for question in self.quiz_dto.questions:
+                for answer in self.answers_dto.answers:
+                    if question.uuid == answer.question_uuid:
+                        right_choices_counter = 0
+                        right_choices_amount = 0
+                        for choice in question.choices:
+                            if choice.is_correct:
+                                right_choices_amount += 1
+                                if choice.uuid in answer.choices:
+                                    right_choices_counter += 1
+                        if right_choices_counter == right_choices_amount:
+                            right_answers += 1
+            return right_answers / len(self.quiz_dto.questions)
+        else:
+            return 0
+
+        # к-во правильных ответов/кол-во вопросов
